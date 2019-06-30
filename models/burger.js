2ac02201ae7b6eec66059e4c498184e562d0786a
James Cutler <jcutler@hubspot.com>
2019-06-30 15:56:30 -0400

    added 'eaten' column to the table

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Burger = sequelize.define('Burger', {
-    burgerName: DataTypes.STRING
+    burgerName: DataTypes.STRING,
+    eaten: DataTypes.BOOLEAN
   }, {});
   Burger.associate = function(models) {
     // associations can be defined here
